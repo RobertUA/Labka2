b8079fce734fed13a83c805bc56f29141a2692bf
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-02-24 11:50:02 +0200

    rob

--- 2-Laba/2-Laba.cpp
+++ 2-Laba/2-Laba.cpp
@@ -1,16 +1,40 @@
 ﻿#include <iostream>
+#include <fstream>
 
 using namespace std;
+
+void readf(string filename, string country[20], int vvv[20][20]);
+
 int main()
 {
-    cout << "MAX LOH\n";
-	cout << "ROBERT LOHHHHHHHHHHHHJHH";
-	string s = "Robert loh";
-	s++;
-	//МАКС ЛОХ
-	//string s ="Robert loh";
-
-
-	//Я НЕ ЛОХ!
+	///						Ет шоб зчитувать з файлу (одного)
+	//int vvv[20][20];
+	//string country[20];
+	//readf("C:\\DDDev\\1.txt", country, vvv);
 	return 0;
+}
+
+void readf(string filename, string country[20], int vvv[20][20])
+{
+	ifstream fin(filename);
+	int c=-1;
+	fin >> c;
+	cout << c << endl;
+	for (int i = 0; i < c; i++)
+	{
+		fin >> country[i];
+		for (int j = 0; j < 20; j++)
+		{
+			fin >> vvv[i][j];
+		}
+	}
+	for (int i = 0; i < c; i++)
+	{
+		cout << country[i];
+		for (int j = 0; j < 20; j++)
+		{
+			cout << " " << vvv[i][j];
+		}
+		cout << endl;
+	}
 }
