91d2c5702d6e621c703daeab63b3b1c2b8d35f7b
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-02-24 12:07:02 +0200

    rob

--- 2-Laba/2-Laba.cpp
+++ 2-Laba/2-Laba.cpp
@@ -8,19 +8,20 @@ void readf(string filename, string country[20], int vvv[20][20]);
 int main()
 {
 	///						Ет шоб зчитувать з файлу (одного)
+	//int kk;
 	//int vvv[20][20];
 	//string country[20];
-	//readf("C:\\DDDev\\1.txt", country, vvv);
+	//readf("C:\\DDDev\\1.txt", country, vvv, kk);
 	return 0;
 }
 
-void readf(string filename, string country[20], int vvv[20][20])
+void readf(string filename, string country[20], int vvv[20][20], int kk)
 {
 	ifstream fin(filename);
 	int c=-1;
 	fin >> c;
 	cout << c << endl;
-	for (int i = 0; i < c; i++)
+	for (int i = 0+kk; i < c+kk; i++)
 	{
 		fin >> country[i];
 		for (int j = 0; j < 20; j++)
@@ -28,4 +29,5 @@ void readf(string filename, string country[20], int vvv[20][20])
 			fin >> vvv[i][j];
 		}
 	}
+	kk += c;
 }
