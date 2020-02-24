7dbe666be3b01a8f69219f3c5a56ba7446375206
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-02-25 00:21:12 +0200

    ROB

--- 2-Laba/2-Laba.cpp
+++ 2-Laba/2-Laba.cpp
@@ -5,49 +5,48 @@
 
 using namespace std;
 
-void readf(string filename, string country[20], int vvv[20][20], int &kk);
+void readf(string filename, string country[20], int vvv[20][20], int& k);
 void countscore(int ctry, int vvv[20][20], int k, int score[20]);
 void top10sort(string country[20], int score[20], int k);
 void topout(string country[20], int score[20], int k);
+void filein(string country[20], int vvv[20][20], int& k);
 
 int main()
 {
-	int kk = 0;
+	int k = 0;
 	int vvv[20][20];
 	int score[20] = { 0 };
 	string country[20];
-
-	WIN32_FIND_DATAA findData;
-	HANDLE hfind;
-	string p;
 	setlocale(0, "ukrainian");
-	cout << "Введiть шлях до папки з вхiдними файлами: ";
-	cin >> p;
-	p += "\\*.csv";
-	
-	hfind = FindFirstFileA(p.c_str(), &findData);
-	if (hfind == INVALID_HANDLE_VALUE)
-	{
-		cout << "Cannot find file" << endl;
-		return -1;
-	}
-	do
-	{
-		string rrr = findData.cFileName;
-		readf(("D:\\Euro\\"+rrr), country, vvv, kk);
-		//cout << findData.cFileName << endl;
-		//cout << rrr << endl;
-	}
-	while (FindNextFileA(hfind, &findData));
-
-	FindClose(hfind);
-
-	for (int i = 0; i < 20; i++) countscore(i, vvv, kk, score);
-	top10sort(country, score, kk);
-	topout(country, score, kk);
+	filein(country, vvv, k);
+	for (int i = 0; i < 20; i++) countscore(i, vvv, k, score);
+	top10sort(country, score, k);
+	topout(country, score, k);
 	return 0;
 }
 
+void filein(string country[20], int vvv[20][20], int &k)
+{
+	WIN32_FIND_DATAA findData;
+	HANDLE hfind;
+	string p;
+	cout << "Введiть шлях до папки з вхiдними файлами: ";
+	cin >> p;
+	string pp = p + "\\*.csv";
+
+	hfind = FindFirstFileA(pp.c_str(), &findData);
+	if (hfind == INVALID_HANDLE_VALUE) cout << "Помилка! Не знайшли файл!" << endl;
+	else
+	{
+		do
+		{
+			string rrr = findData.cFileName;
+			readf((p + "\\" + rrr), country, vvv, k);
+		}
+		while (FindNextFileA(hfind, &findData));
+	}
+	FindClose(hfind);
+}
 void topout(string country[20], int score[20], int k)
 {
 	string ff;
@@ -113,14 +112,14 @@ void countscore(int ctry, int vvv[20][20], int k, int score[20])
 		else if (i >= 2 && i <= 10) score[nv[i][1]] += 10 - i;
 	}
 }
-void readf(string filename, string country[20], int vvv[20][20], int &kk)
+void readf(string filename, string country[20], int vvv[20][20], int &k)
 {
 	ifstream fin(filename);
 	string LINE;
 	string add;
 	int c = -1;
 	fin >> c;
-	for (int i = 0+kk; i < c+kk; i++)
+	for (int i = 0+k; i < c+k; i++)
 	{
 		getline(fin, country[i], ',');
 		for (int j = 0; j < 19; j++)
@@ -131,6 +130,6 @@ void readf(string filename, string country[20], int vvv[20][20], int &kk)
 		getline(fin, LINE, '\n');
 		vvv[i][19] = stoi(LINE);
 	}
-	kk += c;
+	k += c;
 	fin.close();
 }
